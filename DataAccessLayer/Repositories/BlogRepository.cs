b19a566e9b46e499f8c0e5708d35ca4248d71031
Murat Can Çotur <39571221+muratcanctr@users.noreply.github.com>
2022-08-31 15:16:21 +0300

    Creating EntityLayer and DataAccessLayer

---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrate;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        Context c = new Context();
+        public void Add(Blog blog)
+        {
+            c.Add(blog);
+            c.SaveChanges();
+        }
+
+        public void Delete(Blog blog)
+        {
+            c.Remove(blog);
+            c.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            return c.Blogs.Find(id);
+        }
+
+        public List<Blog> GetAll()
+        {
+            return c.Blogs.ToList();
+        }
+
+        public void Update(Blog blog)
+        {
+            c.Update(blog);
+            c.SaveChanges();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal where T : class
+    {
+        
+        public void Add(T entity)
+        {
+            using (var c = new Context())
+            {
+                c.Add(entity);
+                c.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            using var c = new Context();
+            c.Remove(entity);
+        }
+
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Find<T>(id);
+        }
+
+        public List<T> GetAll()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public void Update(T entity)
+        {
+            using var c = new Context();
+            c.Update(entity);
+            c.SaveChanges();
+        }
+    }
+}
